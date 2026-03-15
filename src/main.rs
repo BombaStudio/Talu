98bc10ba883caf9cee4b0c95b0aad3a109cbcf10
islam şahin <124509931+islamfazliyev@users.noreply.github.com>
2026-03-16 01:11:43 +0300

    commit

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,10 @@ use raylib::color::Color;
 mod utils;
 use utils::utils::*;
 
+use crate::draw::draw_register;
+
 mod draw;
 mod input;
-mod pushes; // Yeni modülü dahil et
 
 fn main() {
     let mut engine : WolfEngine = WolfEngine::new();
@@ -23,24 +24,22 @@ fn main() {
 
     let mut title : &str = get_str_slice(&t);
 
-    let (mut rl, thread) = raylib::init() // rl bir RaylibHandle'dır
+    let (mut rl, thread) = raylib::init()
         .size(screen_size_x, screen_size_y)
         .title(title)
         .build();
 
-    pushes::init_input_functions(&mut engine); // Giriş fonksiyonlarını başlat
 
     let code = std::fs::read_to_string("test.wolf").unwrap();
     engine.run(&code);
     engine.get_fn("start", vec![]);
+    draw_register(&mut engine);
+    input::input(&mut engine);
+
 
     while !rl.window_should_close() {
-        // Çizim aşamasını başlat
         let mut d = rl.begin_drawing(&thread);
-
         d.clear_background(Color::RAYWHITE);
-        pushes::init_draw_functions(&mut d, &mut engine); // Çizim fonksiyonlarını döngü içinde başlat
-        d.draw_text("Merhaba Rust ve Raylib!", 190, 200, 20, Color::DARKGRAY);
 
         engine.get_fn("update", vec![]);
     }
